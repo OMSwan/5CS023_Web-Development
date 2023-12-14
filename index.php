74c9d5120b5f03cb17cf16c3add573ccf6587f5c
OMSwan <153859549+OMSwan@users.noreply.github.com>
2023-12-15 00:17:32 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,55 +1,47 @@
-
 <?php
-include("connection/connect.php");
-require_once 'vendor/autoload.php';
+include("../connection/connect.php");
+require_once '../vendor/autoload.php';
+
 error_reporting(0);
 session_start();
 
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/templates');
 $twig = new \Twig\Environment($loader);
 
-// Prepare data to pass to the Twig template
 
 
+// Initialize variables
+$username = '';
+$password = '';
+$error = '';
 
 
-$res = mysqli_query($db, "select * from res_category");
-$categories = [];
-while ($row = mysqli_fetch_array($res)) {
-    $categories[] = $row['c_name'];
+if(isset($_POST['submit']))
+{
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	if(!empty($_POST["submit"])) 
+     {
+	$loginquery ="SELECT * FROM admin WHERE username='$username' && password='$password'";
+	$result=mysqli_query($db, $loginquery);
+	$row=mysqli_fetch_array($result);
+	
+	                        if(is_array($row))
+								{
+                                    	$_SESSION["adm_id"] = $row['adm_id'];
+										header("refresh:1;url=dashboard.php");
+	                            } 
+							else
+							    {
+                                    $error = 'Invalid username or password';
+                                }
+	 }
+	
+	
 }
-
-$query_res = mysqli_query($db, "SELECT * FROM dishes LIMIT 6");
-$dishes = [];
-
-while ($row = mysqli_fetch_array($query_res)) {
-    $dishes[] = $row;
-}
-
-$restaurants = [];
-$ress = mysqli_query($db, "SELECT * FROM restaurant");
-
-while ($rows = mysqli_fetch_array($ress)) {
-    $query = mysqli_query($db, "SELECT * FROM res_category WHERE c_id='" . $rows['c_id'] . "'");
-    $rowss = mysqli_fetch_array($query);
-
-    $restaurants[] = [
-        'category_name' => $rowss['c_name'],
-        'restaurant_logo' => 'admin/Res_img/' . $rows['image'],
-        'restaurant_id' => $rows['rs_id'],
-        'title' => $rows['title'],
-        'address' => $rows['address']
-    ];
-}
-
-
-
+// Render Twig Template
 echo $twig->render('index.twig', [
-    'user_id' => $_SESSION['user_id'], 
-    'categories' => $categories,
-    'restaurants' => $restaurants,
-    'dishes' => $dishes,
-
+        
+    'error' => $error
 ]);
-
-?>
